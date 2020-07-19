951a673b04542ce84713013878a932a26b14ea40
vresgi <valentin77181@gmail.com>
2020-07-19 16:16:18 +0200

    sdkfacade + index

---
+++ App/SdkFacade.php
@@ -0,0 +1,45 @@
+<?php
+namespace App;
+
+class SdkFacade
+{
+    private $sdk;
+
+    private static $declareSdk = [
+        "Cours" => ProviderCours::class,
+    ];
+
+    private $instances;
+
+    public function __construct(array $providers)
+    {
+        session_start();
+
+        foreach ($providers as $provider){
+            $this->instances[$provider["name"]] = new self::$declareSdk[$provider["name"]]($provider["client_id"], $provider["client_secret"]);
+        }
+    }
+
+    public function getLinks(){
+        $links = [];
+
+        foreach ($this->instances as $key => $instance){
+            $links[$key] = $instance->getLink();
+        }
+
+        return $links;
+    }
+
+    public function getUserData(){
+        ["state" => $state] = $_GET;
+
+        $provider = explode("-", $state, 2);
+
+        return $this->instances[array_pop($provider)]->getUserData();
+    }
+
+    public function getCallback(string $path){
+        return $this->sdk->callback($path);
+    }
+
+}
